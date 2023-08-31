476e782efc160c292e8a8db4533e8426249e7268
Vishwanathan27 <vishwanathan.venkataraman27@gmail.com>
2023-09-01 02:08:00 +0530

    edit tags

--- src/components/Form/EditForm/EditForm.js
+++ src/components/Form/EditForm/EditForm.js
@@ -143,8 +143,10 @@ function PostForm() {
               <Button
                 className={Classes.tickBox}
                 onClick={() => {
-                  setTags([...tags, tagName]);
-                  setTagName("");
+                  if (tagName.trim() !== "") {
+                    setTags([...tags, tagName]);
+                    setTagName("");
+                  }
                 }}
               >
                 <Image src="../tick.png" className={Classes.checkmark}></Image>
